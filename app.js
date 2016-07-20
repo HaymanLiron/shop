6d1c5a956873d9182ae0e7a2aa62657217b8a340
Liron <liron.hayman@gmail.com>
2016-07-20 15:38:43 +0300

    made the dashboard look a bit more pretty

--- app.js
+++ app.js
@@ -32,17 +32,46 @@ app.controller("mainController", function ($scope, $http) {
     if (!($scope.products)){ //only do an HTTP request if it's the first time calling the function
         $http.get("https://api.myjson.com/bins/1j3ft")
             .then(function (response) {
+                
                 $scope.products = response.data['storeItems'];
-                console.log($scope.products);
             });
     }
 
     $scope.addBoughtItem = function (x) {
-        x['numBought'] = parseInt(x['numBought']) + 1;
-        x['totalCost'] = parseInt(x['numBought']) * parseInt(x['price']);
-        console.log($scope.products[0].numBought);
-        console.log(x.numBought);
+        x.numBought = parseInt(x.numBought) + 1;
+        x.totalCost = parseInt(x.numBought) * parseInt(x.price);
     };
 
+    $scope.subtractBoughtItem = function (x) {
+        if (x.numBought > 1){
+            x.numBought = parseInt(x.numBought) - 1;
+        }
+        x.totalCost = parseInt(x.numBought) * parseInt(x.price);
+    };
+
+    $scope.subtractBoughtItemDashboard = function (x) {
+        if (x.numBought > 0) {
+            x.numBought = parseInt(x.numBought) - 1;
+        }
+    };
+
+        $scope.deleteBoughtItem = function (x) {
+        x.numBought = 0;
+    }
+
+    $scope.sum = function (property) {
+        //function that sums up all values of a certain property in arr that holds multiple objects
+        var arr = $scope.products;
+        if (arr && arr.hasOwnProperty(length)){
+            var retNum = 0;
+            for (var i = 0; i < arr.length; i++){
+                if (arr[i].hasOwnProperty(property)){
+                    retNum += parseInt(arr[i][property]);
+                }
+            }
+            return retNum;
+        }
+        return 0;
+    }
 
 });
