b3cec23fadcad3c011b5b0b6c734619b6bc1c68e
Liron <liron.hayman@gmail.com>
2016-07-24 13:44:07 +0300

    deleted unnecessary files and folders, modularised some of the angular stuff

--- app.js
+++ app.js
@@ -1,81 +1,29 @@
 /**
  * Created by itc_user on 7/18/2016.
  */
-
-
-var app = angular.module("shopApp", ['ngRoute'])
-    .config(function ($routeProvider) {
-        $routeProvider
-        //route for the home page
-            .when('/', {
-                templateUrl: 'partials/dashboard.html',
-                controller: 'mainController'
-            })
-            //route for the dashboard
-            .when('/dashboard', {
-                templateUrl: 'partials/dashboard.html',
-                controller: 'mainController'
-            })
-            //route for the cart
-            .when('/cart', {
-                templateUrl: 'partials/cart.html',
-                controller: 'mainController'
-            })
-            .when('/individualProduct/:prodId', {
-                templateUrl: 'partials/individualProduct.html',
-                controller: 'mainController'
-            });
-    });
-
-app.controller("mainController", function ($scope, $http, $routeParams) {
-
-    $scope.prodId = $routeParams.prodId;
-
-    if (!($scope.products)) { //only do an HTTP request if it's the first time calling the function
-        $http.get("https://api.myjson.com/bins/3t3od")
-            .then(function (response) {
-                if (!($scope.products)){
-                    $scope.products = response.data['storeItems'];
-                }
-            });
-    }
-
-    $scope.addBoughtItem = function (x) {
-        x.numBought = parseInt(x.numBought) + 1;
-        x.totalCost = parseInt(x.numBought) * parseInt(x.price);
-    };
-
-    $scope.subtractBoughtItem = function (x) {
-        if (x.numBought > 1) {
-            x.numBought = parseInt(x.numBought) - 1;
-        }
-        x.totalCost = parseInt(x.numBought) * parseInt(x.price);
-    };
-
-    $scope.subtractBoughtItemDashboard = function (x) {
-        if (x.numBought > 0) {
-            x.numBought = parseInt(x.numBought) - 1;
-        }
-    };
-
-    $scope.deleteBoughtItem = function (x) {
-        x.numBought = 0;
-        x.totalCost = 0;
-    };
-
-    $scope.sum = function (property) {
-        //function that sums up all values of a certain property in arr that holds multiple objects
-        var arr = $scope.products;
-        if (arr && arr.hasOwnProperty(length)) {
-            var retNum = 0;
-            for (var i = 0; i < arr.length; i++) {
-                if (arr[i].hasOwnProperty(property)) {
-                    retNum += parseInt(arr[i][property]);
-                }
-            }
-            return retNum;
-        }
-        return 0;
-    }
-
-});
+(function () {
+    angular.module("shopApp", ['ngRoute'])
+        .config(function ($routeProvider) {
+            $routeProvider
+            //route for the home page
+                .when('/', {
+                    templateUrl: 'partials/dashboard.html',
+                    controller: 'mainController'
+                })
+                //route for the dashboard
+                .when('/dashboard', {
+                    templateUrl: 'partials/dashboard.html',
+                    controller: 'mainController'
+                })
+                //route for the cart
+                .when('/cart', {
+                    templateUrl: 'partials/cart.html',
+                    controller: 'mainController'
+                })
+                //route for individual products
+                .when('/individualProduct/:prodId', {
+                    templateUrl: 'partials/individualProduct.html',
+                    controller: 'mainController'
+                });
+        });
+})();
--- controllers/controller.js
+++ controllers/controller.js
@@ -2,12 +2,57 @@
  * Created by itc_user on 7/17/2016.
  */
 
+(function () {
+    angular.module("shopApp").controller("mainController", function ($scope, $http, $routeParams) {
 
-app.controller("namesData", function ($scope, stringValue) {
-    $scope.names = ['liron', 'michi', 'ilan', 'noam'];
-    console.log(stringValue);
-});
+        $scope.prodId = $routeParams.prodId;
 
-app.controller("anotherController", function ($scope, myService) {
-    myService.printHi();
-});
+        if (!($scope.products)) { //only do an HTTP request if it's the first time calling the function
+            $http.get("https://api.myjson.com/bins/3t3od")
+                .then(function (response) {
+                    if (!($scope.products)) {
+                        $scope.products = response.data['storeItems'];
+                    }
+                });
+        }
+
+        $scope.addBoughtItem = function (x) {
+            x.numBought = parseInt(x.numBought) + 1;
+            x.totalCost = parseInt(x.numBought) * parseInt(x.price);
+        };
+
+        $scope.subtractBoughtItem = function (x) {
+            if (x.numBought > 1) {
+                x.numBought = parseInt(x.numBought) - 1;
+            }
+            x.totalCost = parseInt(x.numBought) * parseInt(x.price);
+        };
+
+        $scope.subtractBoughtItemDashboard = function (x) {
+            if (x.numBought > 0) {
+                x.numBought = parseInt(x.numBought) - 1;
+            }
+        };
+
+        $scope.deleteBoughtItem = function (x) {
+            x.numBought = 0;
+            x.totalCost = 0;
+        };
+
+        $scope.sum = function (property) {
+            //function that sums up all values of a certain property in arr that holds multiple objects
+            var arr = $scope.products;
+            if (arr && arr.hasOwnProperty(length)) {
+                var retNum = 0;
+                for (var i = 0; i < arr.length; i++) {
+                    if (arr[i].hasOwnProperty(property)) {
+                        retNum += parseInt(arr[i][property]);
+                    }
+                }
+                return retNum;
+            }
+            return 0;
+        }
+
+    });
+})();
